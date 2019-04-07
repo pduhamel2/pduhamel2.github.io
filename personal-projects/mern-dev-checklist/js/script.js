a10914c9c2293b01680d0e51ff8e0653984668d0
Phillip <pjduhamel2@gmail.com>
2019-04-07 13:07:29 -0400

    edited input icon and icon hover

--- personal-projects/mern-dev-checklist/js/script.js
+++ personal-projects/mern-dev-checklist/js/script.js
@@ -1,21 +1,32 @@
-$("ul").on("click", ":checkbox", function(){
-	$(this).parent().toggleClass("completed");
+$('ul').on('click', ':checkbox', function() {
+	$(this).parent().toggleClass('completed');
 });
 
-$("ul").on("click", "span", function(){
-	$(this).parent().fadeOut(200, function(){
+$('ul').on('click', 'span', function() {
+	$(this).parent().fadeOut(200, function() {
 		$(this).remove();
-	})
+	});
 });
 
-$("input[type='text']").keypress(function(event){
-	if(event.which === 13) {
+$("input[type='text']").keypress(function(event) {
+	if (event.which === 13) {
 		var item = $(this).val();
-		$(this).val("");
-		$("ul").append('<li><input type="checkbox">' + item + ' <span><i class="fas fa-trash"></i></span></li>');
+		$(this).val('');
+		$('ul').append('<li><input type="checkbox">' + item + ' <span><i class="fas fa-trash"></i></span></li>');
 	}
 });
 
-$(".fa-plus").click(function() {
+let newItemOpen = true;
+
+$('.newItemToggle').click(function() {
 	$("input[type='text']").fadeToggle();
+	if (newItemOpen) {
+		$(this).removeClass('fa-minus').addClass('fa-plus');
+		newItemOpen = false;
+		console.log('closed');
+	} else {
+		$(this).removeClass('fa-plus').addClass('fa-minus');
+		newItemOpen = true;
+		console.log('opoened');
+	}
 });
